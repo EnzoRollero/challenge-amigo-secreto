cd36a4107aca00a5f1483e05d342360429c35928
Enzo Rollero <enzodanielrollero@gmail.com>
2025-08-05 20:53:32 +0200

    Función para agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigos = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value;
+    console.log(nombre);
+    if (nombre === ''){
+        alert('Por favor, inserte un nombre.');
+    }  else if (nombreAmigos.includes(nombre)){
+        alert('El nombre ingresado se encuentra repetido.');
+        limpiarCajaTexto();
+    } else {
+        nombreAmigos.push(nombre);
+        limpiarCajaTexto();
+    }
+}
+
+function limpiarCajaTexto(){
+    document.querySelector('#amigo').value = '';
+}
+console.log(nombreAmigos);
+
